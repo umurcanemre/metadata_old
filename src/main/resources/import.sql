43d7fdda36c23e6aa3b2360c14f4ace1eaa31843
umurcan emre <ibrahimumurcanemre@hotmail.com>
2019-04-24 18:48:43 +0300

    import sql added

---
+++ src/main/resources/import.sql
@@ -0,0 +1,6 @@
+INSERT INTO `metadata`.`language` (`id`, `label`, `name`) VALUES (1,'en', 'english');
+INSERT INTO `metadata`.`language` (`id`, `label`, `name`) VALUES (2,'de', 'german');
+INSERT INTO `metadata`.`language` (`id`, `label`, `name`) VALUES (3,'esp', 'spanish');
+INSERT INTO `metadata`.`language` (`id`, `label`, `name`) VALUES (4,'it', 'italian');
+INSERT INTO `metadata`.`language` (`id`, `label`, `name`) VALUES (5,'tr', 'turkish');
+INSERT INTO `metadata`.`language` (`id`, `label`, `name`) VALUES (6,'ru', 'russian');
